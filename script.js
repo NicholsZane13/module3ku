12806e0e5a99d9decd278f94a82dfe99574d05e6
nicholsz <nichols.zane16@gmail.com>
2022-10-03 17:22:45 -0500

    Taken out of develop folder and put forward for github

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+// These are the arrays that will be selected for generating a password
 var allCap = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var noCap = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var specChar = ["~","!","@","#","$","%","^","&","*","(",")","_","+","{","}","|","<",">","/",":",";","[","]","|","'","*","?"];
@@ -20,12 +22,10 @@ var passwordArray = [];
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+//This function takes the information from gatherInput function and uses it to create new arrays and transfer them down to the next prompt
 function generatePassword() {
-  /* 
-    We can use object destructuring to unpack all of the values returned
-    by the gatherInput function. 
-    https://www.javascripttutorial.net/es6/javascript-object-destructuring/
-  */ 
+
   var {length, lowers, uppers, numbers, specials} = gatherInput();
   console.log(`
     Length: ${length}
@@ -52,9 +52,10 @@ function generatePassword() {
 
   if (specials === true) {
     var p4Array = p3Array.concat(specChar);
-  };
+  }
+  else var p4Array = p3Array;
 
-
+//This is a function to randomly select strings from the p4Array
   function randomizeArray(arr, num) {
     const jumble = [...arr].sort(() => 0.5 - Math.random());
   
@@ -64,55 +65,28 @@ function generatePassword() {
   
   var finalPassword = (randomizeArray(p4Array, length));
 
+  //This is an array to remove the commas from the array for a cleaner look
   var sikePasswordFinal = (finalPassword.join(""))
 
-  /* 
-    This way we can use the object keys as variable names directly,
-    without having to point to the object name first like this:
-    var inputs = gatherInput()
-    inputs.length
-    inputs.lowers
-    inputs.uppers, etc.
-  */
+
  return sikePasswordFinal;
 }
 
+//This function is where the data from the prompts are stored
 function gatherInput() {
-  /* 
-    The prompt, confirm, and alert methods on the window object
-    function in a specific way that essentially pauses the execution
-    of the rest of the code until a response is given.
-  */
-          // parseInt converts the prompt response from a string to a number
+
     var chosenLength = parseInt(window.prompt("How many characters should your password include?"));
     console.log(typeof chosenLength)
     if (chosenLength < 8 || chosenLength > 128) {
       window.alert("Password must be greater than 8 and no more than 128 characters long!");
       gatherInput()
     }
-    /* 
-      Execution pauses at each prompt until the value is resolved,
-      so we can ask all of our questions at once to get the values
-      we will be working with for the generate function
-    */
-    /* 
-      The confirm methods lets us ask a yes-or-no question, returning
-      true or false, respectively.  Then we can use those conditions in our generate
-      function to determine which types of characters to include.
-    */
+
     var hasLowers = window.confirm("Include lower-case characters?");
     var hasUppers = window.confirm("Include upper-case characters?");
     var hasNumbers = window.confirm("Include numbers?");
     var hasSpecials = window.confirm("Include special characters?");
 
-
-
-
-    
-
-
-
-    /* Package all of the user's choices into a single object to return */
     return {
       length: chosenLength,
       lowers: hasLowers,
